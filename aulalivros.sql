bdde73325311955439133d995d7343b6c13259e3
moniposch <103225033+moniposch@users.noreply.github.com>
2022-05-19 19:20:36 -0300

    Quinto
    Ajuste do php e das tabelas restantes.

---
+++ aulalivros.sql
@@ -0,0 +1,103 @@
+create schema livros;
+
+use livros;
+
+create table livros(
+livro_id int (11) primary key auto_increment not null,
+titulo varchar (255) not null,
+autor varchar (255) not null,
+classificacao varchar (255) not null
+);
+
+create table leitor(
+leitor_id int (11) primary key auto_increment not null,
+nome_leitor varchar (100) not null,
+nascimento date not null,
+qntd_amigos int (4)
+);
+
+create table leitura(
+livro_id int (11),
+leitor_id int (11),
+constraint fk_leituraleitor foreign key (leitor_id) 
+references leitor(leitor_id),
+constraint fk_leituralivro foreign key (livro_id) 
+references livros(livro_id)
+);
+
+create table amizade(
+leitorum_id int (11),
+leitordois_id int (11),
+constraint fk_amizadeleitorum foreign key (leitorum_id) 
+references leitor(leitor_id),
+constraint fk_amizadeleitordois foreign key (leitordois_id) 
+references leitor(leitor_id)
+);
+
+alter table livros add column anopubli int (4) not null;
+
+insert into livros values(null,'Dom Casmurro','Machado de Assis','Romance','1800');
+insert into livros values(null,'Senhor dos Anéis','J. R. R. Tolkien','Infanto-Juvenil','1996');
+insert into livros values(null,'Harry Potter','J. K. Rowling','Infanto-Juvenil','1999');
+insert into livros values(null,'Carrie','Stephen King','Terror','1970');
+insert into livros values(null,'Frankestein','Mary Shelley','Romance','1800');
+
+insert into leitor values(null,'Mônica',str_to_date('15/05/1995','%d/%m/%Y'),'5');
+insert into leitor values(null,'Julianna',str_to_date('02/07/2004','%d/%m/%Y'),'2');
+insert into leitor values(null,'Orlei',str_to_date('26/04/1969','%d/%m/%Y'),'7');
+insert into leitor values(null,'Lúcia',str_to_date('29/11/1970','%d/%m/%Y'),'12');
+
+alter table leitura add column inicio date;
+alter table leitura add column fim date;
+
+select * from livros where titulo like 'Senhor dos Anéis';
+
+insert into leitura values('2','1',str_to_date('01/01/2022','%d/%m/%Y'),str_to_date('20/01/2022','%d/%m/%Y'));
+insert into leitura values('5','4',str_to_date('15/02/2022','%d/%m/%Y'),str_to_date('20/02/2022','%d/%m/%Y'));
+insert into leitura values('1','3',str_to_date('01/04/2022','%d/%m/%Y'),str_to_date('05/04/2022','%d/%m/%Y'));
+insert into leitura values('3','2',str_to_date('08/02/2022','%d/%m/%Y'),str_to_date('07/04/2022','%d/%m/%Y'));
+
+alter table amizade add column solicitacao varchar (100);
+ 
+insert into amizade values('1','2','Confirmada');
+insert into amizade values('1','3','Negada');
+insert into amizade values('2','3','Confirmada');
+insert into amizade values('3','4','Negada');
+
+select l1.nome_leitor leitor1,
+  l2.nome_leitor leitor2,
+  solicitacao
+  from amizade
+  inner join leitor l1 on (l1.leitor_id=amizade.leitorum_id)
+  inner join leitor l2 on (l2.leitor_id=amizade.leitordois_id);
+  
+create table biblioteca(
+biblioteca_id int (11) primary key auto_increment not null,
+livro_id int (11),
+leitor_id int (11),
+constraint fk_bibliotecaleitor foreign key (leitor_id) 
+references leitor(leitor_id),
+constraint fk_livro foreign key (livro_id) 
+references livros(livro_id)
+);
+
+insert into biblioteca values(null,'2','1');
+insert into biblioteca values(null,'3','1');
+insert into biblioteca values(null,'5','1');
+insert into biblioteca values(null,'1','2');
+
+select distinct l1.nome_leitor leitor,
+  l2.titulo livro,
+  inicio,
+  fim
+  from leitura
+  inner join leitor l1 on (l1.leitor_id=leitura.leitor_id)
+  inner join livros l2 on (l2.livro_id=leitura.livro_id);
+
+select l1.nome_leitor leitor,
+  l2.titulo livro
+  from biblioteca
+  inner join leitor l1 on (l1.leitor_id=biblioteca.leitor_id)
+  inner join livros l2 on (l2.livro_id=biblioteca.livro_id);
+
+
